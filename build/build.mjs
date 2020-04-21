818290bfbb0edf68aa52312c6fe160525870ebf2
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-21 16:33:55 -0700

    Packaging

--- build/build.mjs
+++ build/build.mjs
@@ -6,7 +6,7 @@ import { log } from './log.mjs';
 
 const args = mri(process.argv.slice(2), {
   alias: { p: 'path' },
-  default: { path: process.cwd() },
+  default: { path: process.cwd(), package: false },
 });
 
 (async function () {
@@ -27,8 +27,8 @@ const args = mri(process.argv.slice(2), {
     }
   }
 
-  log('create index.mjs containing all helpers');
   if (toExport.size > 0) {
+    log('create index.mjs containing all helpers');
     let output = '';
     const namesToExport = [];
     for (const [filePath, exportedNames] of toExport) {
@@ -38,5 +38,23 @@ const args = mri(process.argv.slice(2), {
 
     output += `export {${namesToExport.join(',')}};`;
     await fs.writeFile(path.join(path.resolve(args.path), 'index.mjs'), output, 'utf8');
+  }
+
+  if (toExport.size > 0 && args.package) {
+    log('update package.json with all helpers');
+
+    const packageJsonFilePath = path.join(process.cwd(), 'package.json');
+    const exportMap = {
+      ".": "./dist/index.mjs",
+    };
+    for (const [filePath, exportedNames] of toExport) {
+      const resolvedFilePath = path.join(path.resolve(args.path), filePath);
+      const relativeFilePath = './' + path.relative(process.cwd(), resolvedFilePath);
+      exportMap[`./${exportedNames}`] = relativeFilePath;
+    }
+
+    const currentPackageJsonContents = JSON.parse(await fs.readFile(packageJsonFilePath, 'utf8'));
+    currentPackageJsonContents.exports = exportMap;
+    await fs.writeFile(packageJsonFilePath, JSON.stringify(currentPackageJsonContents, undefined, 2));
   }
 })();
