116a27e1b843e23488d9db1cae40205de1dd9c38
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-04-22 17:12:34 -0700

    Throw code 5 when error happens in tests

--- test/tests.ts
+++ test/tests.ts
@@ -1,12 +1,13 @@
 import glob from 'fast-glob';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
-import { stdout } from 'process';
+import { stdout, exit } from 'process';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 (async function () {
   const files: Array<string> = await glob(__dirname + '/**/*.test.mjs');
+  let failure = false;
 
   if (files.length > 0) {
     stdout.write('Executing Tests\n');
@@ -20,6 +21,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
           await test();
         } catch (err) {
           stdout.write(` - FAILURE\nErr: ${err}`);
+          failure = true;
           continue;
         }
         stdout.write(' - SUCCESS\n');
@@ -27,5 +29,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
     } catch (e) {
       stdout.write(`TestLoader: failed to execute ${file}, verify its contents include a 'test' export.\n${e}\n`);
     }
+  }
+
+  if (failure) {
+    exit(5);
   }
 })();
